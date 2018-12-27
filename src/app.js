9a05bfc4dfdfa4923dee40bb61e6d05d69359a79
nolefed <42758645+nolefed@users.noreply.github.com>
2018-12-27 17:53:34 -0500

    modified app.js file

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ import './styles/styles.scss'
  
 const store = configureStore();   // creates a stores by passing the reducers. 
 
-store.dispatch(addExpense({ description: 'Water bill', amount:4500 }));   //dispatches action to change the state of the application
-store.dispatch(addExpense({ description: 'Gas bill', createdAt:1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-
-const state = store.getState();    // returns the current state of the redux store
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
       <AppRouter />

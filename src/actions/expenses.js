9a35e2877a9bc2b3db63efdd7d8a6f5beb078256
nolefed <42758645+nolefed@users.noreply.github.com>
2019-01-13 18:43:05 -0500

    store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,7 +8,8 @@ export const addExpense= (expense) => ({
   });
   
   export const startAddExpense = (expenseData = {}) =>{
-    return (dispatch) =>{
+    return (dispatch, getState) =>{
+      const uid = getState().auth.uid;
       const {
       description='', 
       note='',
@@ -18,7 +19,7 @@ export const addExpense= (expense) => ({
 
       const expense = { description, note, amount,createdAt };
 
-      database.ref('expenses').push(expense).then((ref)=>{
+      database.ref(`users/${uid}/expenses`).push(expense).then((ref)=>{
         dispatch(addExpense({
           id:ref.key,
           ...expense
@@ -33,16 +34,18 @@ export const addExpense= (expense) => ({
   });
   
   export const startRemoveExpense = ({ id } = {}) =>{
-    return (dispatch) =>{
-      return database.ref(`expenses/${id}`).remove().then(()=>{
+    return (dispatch,getState) =>{
+      const uid=getState().auth.uid;
+      return database.ref(`users/${uid}/expenses/${id}`).remove().then(()=>{
         dispatch(removeExpense({ id }));
       });
     };
   };
 
   export const startEditExpense =(id,updates) =>{
-    return (dispatch)=>{
-      return database.ref(`expenses/${id}`).update(updates).then(()=>{
+    return (dispatch, getState)=>{
+      const uid= getState().auth.uid;
+      return database.ref(`users/$(uid)/expenses/${id}`).update(updates).then(()=>{
         dispatch(editExpense(id,updates));
       });
     };
@@ -60,8 +63,9 @@ export const addExpense= (expense) => ({
   });
 
   export const startSetExpenses= () =>{
-    return (dispatch) =>{
-     return database.ref('expenses').once('value').then((snapshot)=>{
+    return (dispatch, getState) =>{
+      const uid=getState().auth.uid;
+     return database.ref(`users/${uid}/expenses`).once('value').then((snapshot)=>{
       const expenses=[];
       snapshot.forEach((childSnapshot)=>{
         expenses.push({
